6fba675d0f3e3db417434bfcedc3b8c088c3b6d2
tata.manasa@infocepts.com <tata.manasa@infocepts.com>
2021-08-19 12:14:44 +0000

    snap_demo_1

--- snapshots/orders.sql
+++ snapshots/orders.sql
@@ -1,15 +1,15 @@
-{% snapshot orders %}
-
+{% snapshot orders_snapshot %}
 {{
     config(
-      target_database='PROD',
+      
       target_schema='PUBLIC',
-      unique_key='ID',    
+      unique_key='ID',
+
       strategy='timestamp',
       updated_at='UPDATED_AT',
     )
 }}
 
-select * from {{ref('ordes_mod')};
+select * from {{ ref( 'dbt_demo') }}
 
 {% endsnapshot %}
